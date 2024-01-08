fdd4cad003bdea9453c10f3a8043a176eb991214
makki_d <d.makiuchi@gmail.com>
2024-01-09 01:35:43 +0900

    add example

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 errclass is an error classification utility
 ===========================================
+
+Usage:
+
+```
+// create an ErrClass
+var MyErr = errclass.New("My Error")
+
+// add classification information to the error
+err := MyErr(errors.New("some error"))
+
+// check the error is a MyErr
+if errors.Is(err, MyErr) {
+	fmt.Println("err is an MyErr")
+}
+
+// output:
+// err is an MyErr
+```
---
+++ errclass_test.go
@@ -0,0 +1,27 @@
+package errclass_test
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/makiuchi-d/errclass"
+)
+
+func ExampleErrClass() {
+	// create an ErrClass
+	var MyErr = errclass.New("My Error")
+
+	// add classification information to the error
+	err := MyErr(errors.New("some error"))
+
+	// check the error is a MyErr
+	if errors.Is(err, MyErr) {
+		fmt.Println("err is MyErr")
+	}
+
+	fmt.Printf("err: %v", err)
+
+	// output:
+	// err is MyErr
+	// err: some error
+}
