0cbd3c1b3ea75b8d3572f26775ba0719b18f12e7
makki_d <d.makiuchi@gmail.com>
2024-01-10 00:04:42 +0900

    add tests

--- errclass_test.go
+++ errclass_test.go
@@ -3,6 +3,7 @@ package errclass_test
 import (
 	"errors"
 	"fmt"
+	"testing"
 
 	"github.com/makiuchi-d/errclass"
 )
@@ -24,4 +25,44 @@ func ExampleErrClass() {
 	// output:
 	// err is a MyErr
 	// err: some error
+}
+
+var (
+	MyErr1 = errclass.New("My Error 1")
+	MyErr2 = errclass.New("My Error 2")
+)
+
+func TestWrapped(t *testing.T) {
+	base := errors.New("base error")
+
+	w1 := fmt.Errorf("w1: %w", MyErr1(base))
+	if !errors.Is(w1, MyErr1) {
+		t.Fatal("w1 must be a MyErr1")
+	}
+	if errors.Is(w1, MyErr2) {
+		t.Fatal("w1 must not be a MyErr1")
+	}
+
+	w12 := fmt.Errorf("w12: %w", MyErr2(w1))
+	if !errors.Is(w12, MyErr1) {
+		t.Fatal("w12 must be a MyErr1")
+	}
+	if !errors.Is(w12, MyErr2) {
+		t.Fatal("w12 must be a MyErr2")
+	}
+}
+
+var (
+	samename  = "same name"
+	SameName1 = errclass.New(samename)
+	SameName2 = errclass.New(samename)
+)
+
+func TestSameName(t *testing.T) {
+	err := SameName1(errors.New("base error"))
+	t.Logf("s1:%v, s2:%v", errors.Is(err, SameName1), errors.Is(err, SameName2))
+
+	if errors.Is(err, SameName2) {
+		t.Fatal("err must not be a SameName2")
+	}
 }
